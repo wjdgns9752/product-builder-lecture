d9a7b6b31b039f684e5caf949c1b1722e1632ab4
94swimming <94swimming@gmail.com>
2026-01-13 04:19:20 +0000

    Add Dark/Light mode toggle

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// Add JS here
+const themeToggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
+
+// Check for saved user preference, if any, on load of the website
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+  body.classList.add('dark-mode');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+
+  let theme = 'light';
+  if (body.classList.contains('dark-mode')) {
+    theme = 'dark';
+  }
+  localStorage.setItem('theme', theme);
+});
